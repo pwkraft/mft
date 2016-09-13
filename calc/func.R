bcffcf98fbe19812529effc3ac1a0eea6bd208ba
pwkraft <kraft.pw@gmail.com>
2016-09-13 11:39:46 -0400

    add 2008 again to check new results

--- calc/func.R
+++ calc/func.R
@@ -6,15 +6,100 @@
 ###########################################################################################
 
 ### Load packages
-pkg <- c("reshape2","ggplot2")
+pkg <- c("reshape2","ggplot2","dplyr","quanteda")
 invisible(lapply(pkg, library, character.only = TRUE))
 rm(pkg)
+
+
+### function to pre-process open-ended responses and calculate cosine similarity
+
+mftSimilarity <- function(opend, id, dict, regex){
+  if(nrow(opend) != length(id))          stop("ID vector must be equal to number of observations/documents")
+  if(length(unique(id)) != length(id))  stop("IDs are not unique")
+  
+  ## minor pre-processing
+  spell <- apply(opend, 2, function(x){
+    x <- gsub("(^\\s+|\\s+$)","", x)
+    x[x %in% c("-1 Inapplicable","-7 Refused","N/A","no","none","#(43042)","i am","Nome")] <- ""
+    x <- gsub("//"," ", x , fixed = T)
+    x <- gsub("[[:punct:]]"," ", x)
+    x <- gsub("\\s+"," ", x)
+    x <- gsub("(^\\s+|\\s+$)","", x)
+    return(x)
+  })
+  
+  ## num-lock issue
+  # maybe look into this later
+  
+  ## fix words without whitespace
+  # maybe look into this later
+  
+  ## spell-checking
+  write.table(spell, file = "opend_combined.csv"
+              , sep = ",", col.names = F, row.names = F)
+  spellCheck <- aspell("opend_combined.csv") %>%
+    filter(Suggestions!="NULL")
+  file.remove("opend_combined.csv")
+  
+  ## replace incorrect words
+  for(i in 1:nrow(spellCheck)){
+    spell[spellCheck$Line[i],] <- gsub(spellCheck$Original[i], unlist(spellCheck$Suggestions[i])[1]
+                                          , spell[spellCheck$Line[i],])
+  }
+  
+  ## meta-info: overall response length
+  wc <- apply(spell, 1, function(x){length(unlist(strsplit(x,"\\s+")))})
+  lwc <- log(wc)
+  
+  ## meta-info: number of items answered
+  nitem <- apply(spell != "", 1, sum, na.rm = T)
+  
+  ## aggregate open-ended responses
+  spell <- toLower(apply(spell, 1, paste, collapse = " "))
+  spell <- gsub("NA\\s*","",spell)
+  names(spell) <- id
+  #spell <- spell[spell != ""]
+  
+  ## replace regular expressions with word stems
+  for(i in 1:nrow(regex)){
+    spell <- gsub(regex[i,1], regex[i,2], spell)
+  }
+  
+  ## combine dictionary and responses in common dfm/tfidf
+  spell_tfidf <- corpus(c(dict, spell), docnames = c(names(dict), names(spell))) %>% 
+    dfm() %>% tfidf()
+  
+  ## calculate cosine similarity b/w dictionaries and documents (check pr_DB$get_entries() for options)
+  # normalization is not necessary, cosine similarity is length invariant so results are unchanged
+  sim <- similarity(spell_tfidf, selection = names(dict)
+                    , margin = "documents", method = "cosine") %>% 
+    as.matrix() %>% data.frame()
+  sim$general <- apply(sim,1,sum)
+  
+  ## create scaled variable for moral foundations
+  sim_s <- apply(sim, 2, function(x) scale(x, center=FALSE))
+  colnames(sim_s) <- paste0(colnames(sim_s),"_s")
+  
+  ## dichotmous indicator for moral foundations
+  sim_d <- apply(sim, 2, function(x) as.numeric(x>0))
+  colnames(sim_d) <- paste0(colnames(sim_d),"_d")
+  
+  ## combine similarity results
+  res <- cbind(sim, sim_d, sim_s) %>%
+    mutate(id = as.numeric(rownames(sim))) %>%
+    arrange(id) %>% filter(!is.na(id))
+  
+  ## add spell-checked open-ends
+  out <- data.frame(id, spell=spell, wc=wc, lwc=lwc, nitem=nitem
+                    , stringsAsFactors = F) %>% merge(res)
+  return(out)
+}
 
 
 ### function to plot proportions
 
 prop_plot <- function(data, title, mftvarnames, groupvarname, legendname, file = NULL
-                    , width = par("din")[1], height = par("din")[2]){
+                    , width = par("din")[1], height = par("din")[2], lim = c(0, 0.6)){
     ## prepare dataset
     ci <- function(x){1.96 * sqrt((mean(x, na.rm=T)*(1-mean(x, na.rm=T)))/sum(!is.na(x)))}
 
@@ -45,7 +130,7 @@ prop_plot <- function(data, title, mftvarnames, groupvarname, legendname, file =
         ggtitle(title) + theme_classic(base_size = 8) + theme(panel.border = element_rect(fill=NA)) + 
         geom_hline(yintercept = seq(1.5,4.5,1), col = "lightgrey") +
         guides(color=guide_legend(title=legendname), shape=guide_legend(title=legendname)) +
-        scale_x_continuous(limits = c(0, 0.6)) + theme(legend.position="bottom") +
+        scale_x_continuous(limits = lim) + theme(legend.position="bottom") +
         scale_y_continuous(breaks=1:5, labels=c("Purity / \nSanctity", "Authority / \nRespect"
                                               , "Ingroup / \nLoyalty", "Fairness / \nReciprocity"
                                               , "Harm / \nCare"))
--- calc/prep.R
+++ calc/prep.R
@@ -14,8 +14,11 @@ rm(list=ls())
 ## working directory
 setwd("/data/Dropbox/Uni/Projects/2014/mft/calc")
 
+## load additional functions
+source("func.R")
+
 ## data directory
-datsrc <- "/data/Dropbox/Uni/Data/anes2012/"
+datsrc <- "/data/Dropbox/Uni/Data/"
 
 
 
@@ -24,12 +27,17 @@ datsrc <- "/data/Dropbox/Uni/Data/anes2012/"
 
 
 ## load raw data
-raw2012 <- read.dta13(paste0(datsrc,"anes_timeseries_2012.dta"), convert.factors = F)
+raw2012 <- read.dta13(paste0(datsrc,"anes2012/anes_timeseries_2012.dta"), convert.factors = F)
+raw2008 <- read.dta13(paste0(datsrc,"anes2008/anes_timeseries_2008.dta"), convert.factors = F)
 
 ## respondent id, wave, weight, interview mode (1=FTF, 2=online)
 anes2012 <- data.frame(id=raw2012$caseid, year=2012
                        , weight = raw2012$weight_full
-                       , mode=raw2012$mode)
+                       , mode = raw2012$mode)
+
+anes2008 <- data.frame(id=raw2008$V080001, year=2008
+                       , weight = raw2008$V080101
+                       , mode = 1)
 
 ## ideology (factor/dummies)
 anes2012$ideol <- factor(recode(raw2012$libcpre_self, "1:3=1; 4=2; 5:7=3; else=NA")
@@ -37,11 +45,18 @@ anes2012$ideol <- factor(recode(raw2012$libcpre_self, "1:3=1; 4=2; 5:7=3; else=N
 anes2012$ideol_lib <- as.numeric(anes2012$ideol=="Liberal")
 anes2012$ideol_con <- as.numeric(anes2012$ideol=="Conservative")
 
+anes2008$ideol <- factor(recode(raw2008$V083069, "1:3=1; 4=2; 5:7=3; else=NA")
+                         , labels = c("Liberal","Moderate","Conservative"))
+anes2008$ideol_lib <- as.numeric(anes2008$ideol=="Liberal")
+anes2008$ideol_con <- as.numeric(anes2008$ideol=="Conservative")
+
 ## ideology (continuous, -1 to 1)
 anes2012$ideol_ct <- (recode(raw2012$libcpre_self, "lo:0=NA") - 4)/3
+anes2008$ideol_ct <- (recode(raw2008$V083069, "lo:0=NA") - 4)/3
 
 ## strength of ideology
 anes2012$ideol_str <- abs(anes2012$ideol_ct)
+anes2008$ideol_str <- abs(anes2008$ideol_ct)
 
 ## party identification (factor/dummies)
 anes2012$pid <- factor(recode(raw2012$pid_x
@@ -50,11 +65,19 @@ anes2012$pid <- factor(recode(raw2012$pid_x
 anes2012$pid_dem <- as.numeric(anes2012$pid=="Democrat")
 anes2012$pid_rep <- as.numeric(anes2012$pid=="Republican")
 
+anes2008$pid <- factor(recode(raw2008$V083098x
+                              , "0:1=1; c(2,3,4)=2; 5:6=3; else=NA")
+                       , labels = c("Democrat","Independent","Republican"))
+anes2008$pid_dem <- as.numeric(anes2008$pid=="Democrat")
+anes2008$pid_rep <- as.numeric(anes2008$pid=="Republican")
+
 ## pid continuous
 anes2012$pid_cont <- (recode(raw2012$pid_x, "lo:0=NA") - 4)/3
+anes2008$pid_cont <- (recode(raw2008$V083098x, "lo:-1=NA") - 3)/3
 
 ## strength of partisanship
 anes2012$pid_str <- abs(anes2012$pid_cont)
+anes2008$pid_str <- abs(anes2008$pid_cont)
 
 ## political media exposure
 anes2012$polmedia <- with(raw2012, recode(prmedia_wkinews, "lo:-4=NA; -1=0")
@@ -62,80 +85,127 @@ anes2012$polmedia <- with(raw2012, recode(prmedia_wkinews, "lo:-4=NA; -1=0")
                           + recode(prmedia_wkpaprnws, "lo:-4=NA; -1=0")
                           + recode(prmedia_wkrdnws, "lo:-4=NA; -1=0")) / 28
 
+polmedia <- list(c("V083019","V083024"), c("V083021a", "V083025")
+                 , c("V083021b", "V083023"), c("V083022", "V083026"))
+anes2008$polmedia <- 0
+for(i in 1:length(polmedia)){
+  tmp <- recode(raw2008[,polmedia[[i]][1]], "c(-4,-8,-9)=NA; -1=0")
+  if(length(polmedia[[i]])>1){
+    tmp[raw2008[,polmedia[[i]][1]]==-1] <- recode(raw2008[,polmedia[[i]][2]]
+                                                  , "c(-8,-9,-1)=NA")[raw2008[,polmedia[[i]][1]]==-1]
+  }
+  anes2008$polmedia <- anes2008$polmedia + tmp
+  rm(tmp)
+}
+rm(polmedia)
+
 ## political media exposure (mean centered)
 anes2012$polmedia_c <- scale(anes2012$polmedia, scale=F)
+anes2008$polmedia_c <- scale(anes2008$polmedia, scale=F)
 
 ## political knowledge (factual knowledge questions, pre-election)
 anes2012$polknow <- with(raw2012, ((preknow_prestimes==2) + (preknow_sizedef==1)
                                            + (preknow_senterm==6) + (preknow_medicare==1)
                                            + (preknow_leastsp==1))/5)
+anes2008$polknow <- recode(raw2008$V085119a, "-2=NA; 5=1; else=0")
 
 ## political knowledge (mean centered)
 anes2012$polknow_c <- scale(anes2012$polknow, scale=F)
+anes2008$polknow_c <- scale(anes2008$polknow, scale=F)
 
 ## political discussion
 anes2012$poldisc <- recode(raw2012$discuss_discpstwk, "lo:-1 = NA")/7
 anes2012$poldisc[raw2012$discuss_disc>1] <- 0
 
+anes2008$poldisc <- recode(raw2008$V085108a,"lo:-1 = NA")
+anes2008$poldisc[raw2008$V085108>1] <- 0
+anes2008$poldisc[raw2008$V085108a==-1] <- recode(raw2008$V085109,"lo:-1=NA")[raw2008$V085108a==-1]
+
 ## political discussion (mean centered)
 anes2012$poldisc_c <- scale(anes2012$poldisc, scale=F)
+anes2008$poldisc_c <- scale(anes2008$poldisc, scale=F)
 
 ## candidate evaluations (feeling thermometer)
 anes2012$eval_cand <- (recode(raw2012$ft_dpc, "lo:-1=NA; 101:hi=NA") - 
                          recode(raw2012$ft_rpc, "lo:-1=NA; 101:hi=NA"))
 
+anes2008$eval_cand <- (recode(raw2008$V083037a, "lo:-1=NA; 101:hi=NA") - 
+                         recode(raw2008$V083037b, "lo:-1=NA; 101:hi=NA"))
+
 ## party evaluations (feeling thermometer)
 anes2012$eval_party <- (recode(raw2012$ft_dem, "lo:-1=NA; 101:hi=NA") -
                           recode(raw2012$ft_rep, "lo:-1=NA; 101:hi=NA"))
 
+anes2008$eval_party <- (recode(raw2008$V083044a, "lo:-1=NA; 101:hi=NA") -
+                          recode(raw2008$V083044b, "lo:-1=NA; 101:hi=NA"))
+
 ## voted in previous election
 anes2012$pastvote <- recode(raw2012$interest_voted2008, "c(2,5)=0; lo:-1=NA")
+anes2008$pastvote <- recode(raw2008$V083007, "c(2,5)=0; lo:-1=NA")
 
 ## voted in current election
 anes2012$vote <- recode(raw2012$rvote2012_x, "2=0; lo:-1=NA")
+anes2008$vote <- recode(raw2008$V085036x, "2=0; lo:-1=NA")
 
 ## voted for democratic presidential candidate
 anes2012$vote_dem <- recode(raw2012$presvote2012_x, "2=0; c(-2,5)=NA")
+anes2008$vote_dem <- recode(raw2008$V083169a, "2=0; c(-2,5)=NA")
 
 ## participated in protest march / rally
 anes2012$protest <- recode(raw2012$dhsinvolv_march, "c(2,5)=0; lo:-1=NA")
+anes2008$protest <- recode(raw2008$V085201a, "c(2,5)=0; lo:-1=NA")
 
 ## signed a petition
 anes2012$petition <- as.numeric((recode(raw2012$dhsinvolv_netpetition, "c(2,5)=0; lo:-1=NA") +
                                    recode(raw2012$dhsinvolv_petition, "c(2,5)=0; lo:-1=NA")) > 0)
 
+anes2008$petition <- as.numeric((recode(raw2008$V085201c, "c(2,5)=0; lo:-1=NA") +
+                                   recode(raw2008$V085201d, "c(2,5)=0; lo:-1=NA")) > 0)
+
 ## wear a campaign button
 anes2012$button <- recode(raw2012$mobilpo_sign, "c(2,5)=0; lo:-1=NA")
+anes2008$button <- recode(raw2008$V085031, "c(2,5)=0; lo:-1=NA")
 
 ## additive index protest behavior
 anes2012$part <- with(anes2012, as.numeric((protest + petition + button)>0))
+anes2008$part <- with(anes2008, as.numeric((protest + petition + button)>0))
 
 ## age
 anes2012$age <- recode(raw2012$dem_age_r_x, "c(-2,-9,-8) = NA")
+anes2008$age <- recode(raw2008$V081104, "c(-2,-9,-8) = NA")
 
 ## sex
 anes2012$female <- raw2012$gender_respondent_x - 1
+anes2008$female <- raw2008$V081101 - 1
 
 ## race
 anes2012$black <- as.numeric(recode(raw2012$dem_raceeth_x, "lo:0 = NA") == 2)
+anes2008$black <- as.numeric(recode(raw2008$V081102, "lo:0 = NA") == 2)
 
 ## religiosity (church attendance)
 anes2012$relig <- (5 - recode(raw2012$relig_churchoft, "lo:0 = NA"))/5
 anes2012$relig[raw2012$relig_church != 1] <- 0
 anes2012$relig[raw2012$relig_churchwk == 2] <- 1
 
+anes2008$relig <- (5 - recode(raw2008$V083186a, "lo:0 = NA"))/5
+anes2008$relig[raw2008$V083186 != 1] <- 0
+anes2008$relig[raw2008$V083186b == 2] <- 1
+
 ## education (bachelor degree)
-anes2012$educ <- as.numeric(raw2012$dem_edugroup_x >= 4)
-anes2012$educ[raw2012$raw2012$dem_edugroup_x < 0] <- NA
+anes2012$educ <- recode(raw2012$dem_edugroup_x, "lo:-1=NA; 0:3=0; 3:hi=1")
+anes2008$educ <- recode(raw2008$V083218x, "lo:-1=NA; 1:5=0; 6:hi=1")
 
 ## education (continuous)
-anes2012$educ_cont <- (recode(raw2012$dem_edugroup_x, "lo:0=NA") - 1)/4
+anes2012$educ_cont <- (recode(raw2012$dem_edugroup_x, "lo:-1=NA") - 1)/4
+anes2008$educ_cont <- recode(raw2008$V083218x, "lo:-1=NA")/7
 
 ## spanish speaking respondent
 anes2012$spanish <- as.numeric(raw2012$profile_spanishsurv == 1 |
                                  raw2012$admin_pre_lang_start == 2 |
                                  raw2012$admin_post_lang_start == 2)
 
+anes2008$spanish <- as.numeric(raw2008$V082011 == 2 |
+                                 raw2008$V082011 == 3)
 
 
 ###############################
@@ -143,97 +213,35 @@ anes2012$spanish <- as.numeric(raw2012$profile_spanishsurv == 1 |
 
 
 ## read original open-ended responses (downloaded from anes website)
-anes2012opend <- read.csv(paste0(datsrc,"anes2012TS_openends.csv"), as.is = T) %>%
+anes2012opend <- read.csv(paste0(datsrc,"anes2012/anes2012TS_openends.csv"), as.is = T) %>%
   select(caseid, candlik_likewhatdpc, candlik_dislwhatdpc, candlik_likewhatrpc, candlik_dislwhatrpc
          , ptylik_lwhatdp, ptylik_dwhatdp, ptylik_lwhatrp, ptylik_dwhatrp)
 
-## minor pre-processing
-anes2012spell <- apply(anes2012opend[,-1], 2, function(x){
-  x <- gsub("(^\\s+|\\s+$)","", x)
-  x[x %in% c("-1 Inapplicable","-7 Refused","N/A","no","none","#(43042)","i am","Nome")] <- ""
-  x <- gsub("//"," ", x , fixed = T)
-  x <- gsub("[[:punct:]]"," ", x)
-  x <- gsub("\\s+"," ", x)
-  x <- gsub("(^\\s+|\\s+$)","", x)
-  return(x)
-})
-
-## num-lock issue
-# maybe look into this later
-
-## fix words without whitespace
-# maybe look into this later
-
-## spell-checking
-write.table(anes2012spell, file = "out/anes2012TS_combined.csv"
-            , sep = ",", col.names = F, row.names = F)
-spell <- aspell("out/anes2012TS_combined.csv") %>%
-  filter(Suggestions!="NULL")
-
-## replace incorrect words
-for(i in 1:nrow(spell)){
-  anes2012spell[spell$Line[i],] <- gsub(spell$Original[i], unlist(spell$Suggestions[i])[1]
-                                        , anes2012spell[spell$Line[i],])
-}
-anes2012spell <- data.frame(id = anes2012opend$caseid, anes2012spell,stringsAsFactors = F)
-
-
-### add meta information about responses
-
-## overall response length
-anes2012$wc <- apply(anes2012spell[,-1], 1, function(x){
-  length(unlist(strsplit(x,"\\s+")))
-})
-anes2012$lwc <- log(anes2012$wc)
-
-## number of items answered
-anes2012$nitem <- apply(anes2012spell[,-1] != "", 1, sum, na.rm = T)
-
-
-### calculate cosine similarity between dictionaries and documents
+anes2008opend <- read.csv(paste0(datsrc,"anes2008/anes2008TSopenends_redacted_Dec2012Revision.csv"), as.is = T) %>%
+  select(caseid, DemPC_like, DemPC_dislike, RepPC_like, RepPC_dislike
+         , DemParty_like, DemParty_dislike, RepParty_like, RepParty_dislike)
 
 ## load dictionary
 dict <- sapply(c("authority","fairness","harm","ingroup","purity"), function(x){
   read.csv(paste0("in/graham/",x,"_noregex.csv")) %>%
     sapply(paste, collapse = " ") %>% as.character()
 })
+
+## regex replacements for dictionary
 dict_df <- sapply(c("authority","fairness","harm","ingroup","purity"), function(x){
   cbind(read.csv(paste0("in/graham/",x,".csv"), allowEscapes = T, stringsAsFactors = F)[[1]]
         , read.csv(paste0("in/graham/",x,"_noregex.csv"), stringsAsFactors = F)[[1]])
 }) %>% do.call("rbind", .)
 
-## load open-ended responses
-anes2012resp <- toLower(apply(anes2012spell[,-1], 1, paste, collapse = " "))
-anes2012resp <- gsub("NA\\s*","",anes2012resp)
-names(anes2012resp) <- anes2012spell$id
-anes2012resp <- anes2012resp[anes2012resp != ""]
-for(i in 1:nrow(dict_df)){
-  anes2012resp <- gsub(dict_df[i,1],dict_df[i,2],anes2012resp)
-}
+## pre-process open-ended data and calculate similarity
+anes2012sim <- mftSimilarity(opend = anes2012opend[-1], id = anes2012opend$caseid
+                             , dict = dict, regex = dict_df)
 
-## combine dictionary and responses in common dfm/tfidf
-anes2012tfidf <- corpus(c(dict,anes2012resp)
-                        , docnames = c("authority","fairness","harm","ingroup","purity"
-                                       , names(anes2012resp))) %>% dfm() %>% tfidf()
+anes2008sim <- mftSimilarity(opend = anes2008opend[-1], id = anes2008opend$caseid
+                             , dict = dict, regex = dict_df)
 
-## calculate similarity (check pr_DB$get_entries() for options)
-# normalization is not necessary, cosine similarity is length invariant so results are unchanged
-anes2012sim <- similarity(anes2012tfidf
-                          , selection =  c("harm","fairness","ingroup","authority","purity")
-                          , margin = "documents", method = "cosine") %>% as.matrix() %>% data.frame()
-anes2012sim$general <- apply(anes2012sim,1,sum)
-anes2012sim <- cbind(anes2012sim, apply(anes2012sim, 2, function(x) as.numeric(x>0)))
-colnames(anes2012sim)[7:12] <- paste0(colnames(anes2012sim)[7:12],"_d")
-anes2012sim$id <- as.numeric(rownames(anes2012sim))
-anes2012sim <- anes2012sim %>% arrange(id) %>% filter(!is.na(id))
-
-## create scaled variables for moral foundations (throws an error if run twice, not sure why)
-anes2012sim <- mutate(anes2012sim, harm_s = scale(harm, center = FALSE), fairness_s = scale(fairness, center = FALSE)
-                   , ingroup_s = scale(ingroup, center = FALSE), authority_s = scale(authority, center = FALSE)
-                   , purity_s = scale(purity, center = FALSE), general_s = scale(general, center = FALSE))
-
-
-### merge ts data and open-ended data and save objects for analyses
-
+## merge ts data and open-ended data and save objects for analyses
 anes2012 <- merge(anes2012, anes2012sim)
-save(anes2012, anes2012opend, anes2012spell, file="out/anes.RData")
+anes2008 <- merge(anes2008, anes2008sim)
+
+save(anes2012, anes2012opend, anes2008, anes2008opend, file="out/anes.RData")
